8a83c9cc94f5c77b264f510e7aee19a39d943595
coleszka <symon@interia.eu>
2019-01-25 14:46:07 +0100

    added log-out

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
 
--- user-menu.php
+++ user-menu.php
@@ -16,7 +16,6 @@ require_once ('php/session/check_log_in.php');
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
-    <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
 
 </head>
@@ -49,13 +48,14 @@ require_once ('php/session/check_log_in.php');
                     <img src="image/profile.png">
                 </li>
                 <li>
-                    <div class="dropdown show">
-                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
-
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <div style="margin-left: 3px;" class="btn-group">
+                        <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php echo $_SESSION['login']; ?>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
                             <a class="dropdown-item" href="#">Action</a>
                             <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="php/log-out/log-out.php">Wyloguj</a>
                         </div>
                     </div>
                 </li>
@@ -67,7 +67,11 @@ require_once ('php/session/check_log_in.php');
 <!-- Page Content -->
 <div class="container">
 
+    <div class="user-content">
 
+
+
+    </div>
 
     <div id="wrapper">
 
@@ -96,7 +100,9 @@ require_once ('php/session/check_log_in.php');
     <!-- /.container -->
 </footer>
 
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
 
 </html>
